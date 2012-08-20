3c02919019cce7fcc4802c76d778dbf982f4c5d5
Lyndon Maydwell <maydwell@gmail.com>
2012-08-20 17:34:36 +0800

    Removing hmatrix stuff from linreg

--- LinReg.hs
+++ LinReg.hs
@@ -28,27 +28,3 @@ zipPds = mconcat
 
 lsr :: Int -> Data -> [Double]
 lsr = undefined
-
-
--- Playing with HMatrix stuff
-
-solve_linear :: PartialDerivitives -> ConstantTerms
-solve_linear = undefined
-
-reduce :: Int -> SimpleSum -> ConstantTerms
-reduce o s = solve_linear $ partial_derivatives o s
-
-solve_for_order :: Int -> Environment -> ConstantTerms
-solve_for_order o e = reduce o $ normal e (polynomial_of_order o)
-
-coefs :: Matrix Double
-coefs = (4><4) [ 1, 2, 3, 3
-               , 4, 5, 6, 2
-               , 4, 5, 6, 2
-               , 7, 8, 9, 1]
-
-vals :: Matrix Double
-vals = (4><1) [ 1, 2, 3, 4 ]
-
-answer :: Matrix Double
-answer = linearSolveLSR coefs vals
